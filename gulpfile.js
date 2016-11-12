7d9643294e2439f24854ad6d32987725f57af449
mraerino <melodie124@gmail.com>
2016-11-12 15:59:02 +0100

    Add polymer build task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,26 @@
 
 var gulp = require('gulp');
 var del = require('del');
+const PolymerProject = require('polymer-build').PolymerProject;
+const mergeStream = require('merge-stream');
 var $ = require('gulp-load-plugins')();
+
+const distDir = 'build';
+
+var watches = [];
+
+gulp.task('polymer', function() {
+    const project = new PolymerProject(require('./polymer.json'));
+
+    return mergeStream(project.sources(), project.dependencies())
+        .pipe(project.analyzer)
+        .pipe(project.bundler)
+        .pipe(gulp.dest(distDir));
+});
+watches.push({
+    src: "elements/**/*",
+    tasks: 'polymer'
+});
 
 gulp.task('generate-icons', ['clean-icons'], function() {
     return gulp.src('images/icon.png')
