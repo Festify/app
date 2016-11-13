a78b1874e1b5183c5e248ce1fabd1f7078479571
Leo Bernard <admin@leolabs.org>
2016-11-13 16:49:36 +0100

    Implement Cordova build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,41 @@ const runSequence = require('run-sequence');
 const PolymerProject = require('polymer-build').PolymerProject;
 const mergeStream = require('merge-stream');
 const browserSync = require('browser-sync').create();
+const crisper = require('gulp-crisper');
+const template = require('gulp-template');
 const historyApiFallback = require('connect-history-api-fallback');
 const $ = require('gulp-load-plugins')();
+const cordova = require("cordova-lib").cordova;
 
-const distDir = 'www';
+const distDir = 'build';
+const appDir = 'www';
 
 var watches = [];
 
 gulp.task('clean', function() {
-    return del(distDir);
+    return del([distDir, appDir]);
 });
 
-gulp.task('polymer', function () {
-    const project = new PolymerProject(require('./polymer.json'));
-
+function polymerBuilding(project) {
     return mergeStream(project.sources(), project.dependencies())
         .pipe(project.analyzer)
         .pipe($.if('index.html', $.useref()))
-        .pipe(project.bundler)
+        .pipe(project.bundler);
+}
+
+gulp.task('polymer', function () {
+    const project = new PolymerProject(require('./polymer.json'));
+    return polymerBuilding(project)
         .pipe(gulp.dest(distDir));
 });
+
+gulp.task('polymer-cordova', function() {
+    const projectCordova = new PolymerProject(require('./polymer-cordova.json'));
+    return polymerBuilding(projectCordova)
+        .pipe($.if('elements/app-shell.html', crisper()))
+        .pipe(gulp.dest(appDir))
+});
+
 watches.push({
     src: "elements/**/*",
     tasks: browserSync.reload
@@ -39,7 +54,7 @@ gulp.task('generate-icons-ios', function() {
                 return {width: width, rename: {suffix: '-' + width}};
             })
         }))
-        .pipe(gulp.dest(path.join(distDir, 'images/manifest')));
+        .pipe(gulp.dest(path.join(appDir, 'images/manifest')));
 });
 
 gulp.task('generate-icons', ['generate-icons-ios'], function () {
@@ -76,10 +91,28 @@ gulp.task('serve', function () {
     });
 });
 
+
+gulp.task("package-cordova", function (callback) {
+    cordova.build({
+        "platforms": ["ios"],
+        "options": {
+            argv: []
+        }
+    }, callback);
+});
+
 gulp.task('build', function(cb) {
     runSequence(
         'clean',
         ['polymer', 'generate-icons'],
+        cb
+    );
+});
+
+gulp.task('build-cordova', function(cb) {
+    runSequence(
+        'clean',
+        ['polymer-cordova', 'generate-icons', 'package-cordova'],
         cb
     );
 });
