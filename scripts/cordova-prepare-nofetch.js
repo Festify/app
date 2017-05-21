0b9f8cac5af212dc591f7c14a7e02dd7423fe234
mraerino <melodie124@gmail.com>
2017-05-22 01:00:28 +0200

    :construction_worker: Fix cordova preparation when plugin from git
    Cordova 7 always fetches plugins via NPM so I built a script that uses the old method automagically

---
+++ scripts/cordova-prepare-nofetch.js
@@ -0,0 +1,16 @@
+const cordova = require('cordova-lib').cordova.raw;
+const difference = require('lodash').difference;
+
+module.exports = function(context) {
+    const plugins = require('../package.json').cordova.plugins;
+    const pluginsPresent = context.opts.cordova.plugins;
+    const installPlugins = difference(Object.keys(plugins), pluginsPresent)
+        .filter(key => 'spec' in plugins[key]);
+
+    return installPlugins.reduce((p, plugin) => {
+        console.log(`Installing ${plugin} from git...`);
+        return p.then(() => cordova.plugin('add', plugins[plugin].spec, {
+            argv: ['--nofetch', '--nosave']
+        }));
+    }, Promise.resolve());
+};
