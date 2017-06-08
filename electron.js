5290dd80cb9f52c92b23d3977c23a835180d0d14
leolabs <admin@leolabs.org>
2017-06-09 01:04:32 +0200

    :sparkles: Add Desktop Support :tada:

---
+++ electron.js
@@ -0,0 +1,39 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+const startSpotify = require('start-spotify');
+
+let win;
+
+function createWindow () {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        backgroundColor: '#1c1f24',
+        title: 'Festify'
+    });
+
+    startSpotify()
+        .then(() => win.loadURL(url.format({
+            pathname: path.join(__dirname, 'www', 'app.html'),
+            protocol: 'file:',
+            slashes: true
+        })))
+        .catch(() => win.loadURL(url.format({
+            pathname: path.join(__dirname, 'www', 'spotify-missing.html'),
+            protocol: 'file:',
+            slashes: true
+        })));
+
+    win.on('closed', () => win = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => app.quit());
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+});
