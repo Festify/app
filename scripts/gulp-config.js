65f2532d5149b330aa2a629bb6acea17b3dd0018
Marcus Weiner <marcus.weiner@gmail.com>
2017-11-01 21:22:37 +0100

    :art: Move electron builder config into file

--- scripts/gulp-config.js
+++ scripts/gulp-config.js
@@ -1,25 +1,8 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+
 module.exports = {
-    electron: {
-        appId: 'rocks.festify.app',
-        win: {
-            publish: ['github'],
-            icon: 'www/images/icon.ico'
-        },
-        mac: {
-            publish: ['github'],
-            icon: 'www/images/icon.icns',
-            category: 'public.app-category.music'
-        },
-        dmg: {
-            title: 'Festify',
-            background: 'www/images/background.png'
-        },
-        appx: {
-            backgroundColor: '#1c1f24',
-            displayName: 'Festify',
-            publisherDisplayName: 'Festify Dev Team'
-        }
-    },
+    electron: yaml.safeLoad(fs.readFileSync('electron-builder.yml')),
     envFiles: [
         '.env',
         'google-services.json',
