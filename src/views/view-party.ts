1f316c6c977ce3ccb461f438bde550668057de67
Marcus Weiner <marcus.weiner@gmail.com>
2017-12-27 02:29:18 +0100

    :fire: Cleanup reused view

--- src/views/view-party.ts
+++ src/views/view-party.ts
@@ -96,10 +96,7 @@ const PartyView = (props: PartyViewProps & PartyViewDispatch) => html`
 
     <app-drawer-layout fullbleed>
         <app-drawer slot="drawer">
-            <queue-drawer page="{{page}}"
-                          state="[[state]]"
-                          tv-url="[[_castUrl]]">
-            </queue-drawer>
+            <queue-drawer></queue-drawer>
         </app-drawer>
 
         <div>
@@ -108,64 +105,13 @@ const PartyView = (props: PartyViewProps & PartyViewDispatch) => html`
                     <paper-icon-button icon="menu" drawer-toggle></paper-icon-button>
                     <div main-title>[[state.party.name]]</div>
                 </app-toolbar>
-                <search-bar value="{{searchQuery}}" placeholder="Add Tracks"></search-bar>
-                <playback-progress-bar duration="[[_getTrackDuration(state.*, _trackMeta.*)]]"
-                                       playback="[[state.party.playback]]">
-                </playback-progress-bar>
+                <search-bar placeholder="Add Tracks"></search-bar>
+                <playback-progress-bar></playback-progress-bar>
             </header>
 
             <main>
                 ${Body(props.view)}
             </main>
-            <iron-pages selected="[[page]]"
-                        attr-for-selected="view">
-                <div class="track-list" view="queue">
-                    <div id="skipBackground"
-                         style$="display: [[_getDisplaySkipIndicator(tracks.length, state.isOwner)]]">
-                        <p id="skipIndicator">Skip</p>
-                    </div>
-
-                    <dom-flip id="trackAnimator">
-                        <template is="dom-repeat"
-                                  id="trackList"
-                                  items="[[tracks]]"
-                                  on-dom-change="_trackListChange">
-                            <queue-track data-flip-id$="[[_generateFbId(item)]]"
-                                         metadata="[[_trackMeta]]"
-                                         state="[[state]]"
-                                         track="[[item]]"
-                                         user-votes="[[_userVotes]]">
-                            </queue-track>
-                        </template>
-                    </dom-flip>
-
-                    <p>
-                        <paper-button hidden$="[[_getDisplayLoadRestButton(tracks.length, _trackLimit)]]"
-                                      on-tap="_loadAllTracks"
-                                      raised>
-                            There's more! Load the rest
-                        </paper-button>
-                    </p>
-                </div>
-
-                <div class="track-list" view="search">
-                    <template is="dom-repeat" items="[[_searchResults]]">
-                        <queue-track metadata="[[_searchMeta]]"
-                                     state="[[state]]"
-                                     track="[[item]]"
-                                     user-votes="[[_userVotes]]">
-                        </queue-track>
-                    </template>
-                </div>
-
-                <view-settings view="settings"
-                               is-loading="[[_playlistsLoading]]"
-                               playlists="[[_playlists]]"
-                               state="{{state}}">
-                </view-settings>
-
-                <view-share view="share" state="[[state]]"></view-share>
-            </iron-pages>
         </div>
     </app-drawer-layout>
 `;
