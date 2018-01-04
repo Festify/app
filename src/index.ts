d076db1ae0bbbe336bc4ef00a2ef2e0ac2866b42
Moritz Gunz <moritz.gunz@gmail.com>
2018-01-04 15:11:52 +0100

    :wrench: Switch to DefinitelyTyped types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:ordered-imports */
 
-import '@festify/spotify-web-playback-sdk-types';
+import '@types/spotify-web-playback-sdk';
 
 import './store';
 import './views/app-shell';
