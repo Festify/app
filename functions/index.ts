ce991bbc38d185e01c18d20188352b9868d735fd
Moritz Gunz <moritz.gunz@gmail.com>
2018-02-08 18:07:18 +0100

    :art: Group cloud functions into groups

--- functions/index.ts
+++ functions/index.ts
@@ -7,12 +7,9 @@ import voteProcessor from './lib/vote-processor';
 firebase.initializeApp(functions.config().firebase);
 
 export const clientToken = functions.https.onRequest(spotifyAuth.clientToken);
-
 export const exchangeCode = functions.https.onRequest(spotifyAuth.exchangeCode);
-
 export const exchangeCodeProtocol = functions.https.onRequest(spotifyAuth.exchangeCodeProtocol);
+export const refreshToken = functions.https.onRequest(spotifyAuth.refreshToken);
 
 export const processVotes = functions.database.ref('/votes/{partyId}/{trackId}/{userId}')
     .onWrite(voteProcessor);
-
-export const refreshToken = functions.https.onRequest(spotifyAuth.refreshToken);
