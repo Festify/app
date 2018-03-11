d5dc175f2d8025d8e4c2f8edc8cbec5217524eff
Moritz Gunz <moritz.gunz@gmail.com>
2018-03-11 22:50:29 +0100

    :hammer: Don't mutate action object
    Co-Authored-By: Leo Bernard <admin@leolabs.org>

--- src/sagas/playback-state.ts
+++ src/sagas/playback-state.ts
@@ -75,16 +75,16 @@ function* handleFirebase(partyId: string) {
         yield takeEveryWithState(
             Types.UPDATE_PLAYBACK_STATE,
             playbackSelector,
-            function* ({payload}: UpdatePlaybackStateAction, oldState: Playback, newState: Playback) {
-                delete payload.last_change;
-
-                const update =
-                    oldState.playing !== newState.playing || oldState.last_position_ms !== newState.last_position_ms
-                    ? {
-                        ...payload,
-                        last_change: firebaseNS.database!.ServerValue.TIMESTAMP,
-                    }
-                    : payload;
+            function* ({ payload }: UpdatePlaybackStateAction, oldState: Playback, newState: Playback) {
+                // Exclude last_change for non-playback-state-changing updates to prevent
+                // infinite update loop
+                const { last_change, ...rest } = payload;
+                const isChangingState = oldState.playing !== newState.playing ||
+                    oldState.last_position_ms !== newState.last_position_ms;
+                const update = isChangingState ? {
+                    ...payload,
+                    last_change: firebaseNS.database!.ServerValue.TIMESTAMP,
+                } : rest;
 
                 playbackRef.update(update);
             },
