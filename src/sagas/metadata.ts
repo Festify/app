355300e77d93494c0b7f8559297e01f12a6a0a4e
Moritz Gunz <moritz.gunz@gmail.com>
2018-03-18 02:02:30 +0100

    :bug: Fix access to possibly undefined field

--- src/sagas/metadata.ts
+++ src/sagas/metadata.ts
@@ -81,17 +81,24 @@ function* loadFanartForNewTracks(_) {
                 continue;
             }
 
-            const backgroundImages: string[] = (yield fanartResponse.json()).artistbackground.map(bg => bg.url)
-                .map(url => {
-                    // Proxy fanart through imgix to apply server-side optimizations and blurring (client
-                    // side-blurring kills performance quite extremely).
-                    const u = new URL(url);
-                    u.host = 'festify-fanart.imgix.net';
-                    u.protocol = 'https';
+            const bg = (yield fanartResponse.json()).artistBackground;
+            if (!bg || !bg.length) {
+                yield put(updateMetadata(empty()));
+                continue;
+            }
 
-                    return `${u.toString()}?blur=100&auto=compress,format`;
-                });
-            yield put(updateMetadata({ [trackId]: backgroundImages }));
+            yield put(updateMetadata({
+                [trackId]: bg.map(bg => bg.url)
+                    .map(url => {
+                        // Proxy fanart through imgix to apply server-side optimizations and blurring (client
+                        // side-blurring kills performance quite extremely).
+                        const u = new URL(url);
+                        u.host = 'festify-fanart.imgix.net';
+                        u.protocol = 'https';
+
+                        return `${u.toString()}?blur=100&auto=compress,format`;
+                    })
+            }));
         } catch (err) {
             console.error(`Failed to fetch fanart for '${metadata.name}'.`, err);
         }
