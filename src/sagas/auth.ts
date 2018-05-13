4ccdb64d84a00dbeed3f883fce41c3daa7fe9553
Moritz Gunz <moritz.gunz@gmail.com>
2018-05-13 17:31:30 +0200

    :fire: Remove unused imports

--- src/sagas/auth.ts
+++ src/sagas/auth.ts
@@ -1,6 +1,4 @@
 import { User } from '@firebase/auth-types';
-import { HttpsCallableResult, HttpsError } from '@firebase/functions-types';
-import { replace, LOCATION_CHANGED } from '@mraerino/redux-little-router-reactless';
 import { delay } from 'redux-saga';
 import { apply, call, fork, put, takeEvery } from 'redux-saga/effects';
 
