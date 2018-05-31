e5c01376c7ba2aed511217b245106e8abd79c122
Moritz Gunz <moritz.gunz@gmail.com>
2018-05-31 17:34:33 +0200

    :sparkles: Compile SystemJS output to ES5
    This is currently done less optimally than possible but works alright.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
+import minifyLit from '@mraerino/rollup-plugin-minifyliterals';
 import historyApi from 'connect-history-api-fallback';
 import * as fs from 'fs';
+import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
+import browsersync from 'rollup-plugin-browsersync';
 import cjs from 'rollup-plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 import nodeBuiltins from 'rollup-plugin-node-builtins';
 import nodeGlobals from 'rollup-plugin-node-globals';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
-import minifyLit from '@mraerino/rollup-plugin-minifyliterals';
-import browsersync from 'rollup-plugin-browsersync';
 import replace from 'rollup-plugin-replace';
+import typescript from 'rollup-plugin-typescript2';
+import { uglify } from 'rollup-plugin-uglify';
 import path from 'path';
 
 const distTarget = './build';
@@ -24,7 +26,7 @@ if (!fs.existsSync('build')) {
     fs.mkdirSync('build');
 }
 
-const plugins = [
+const basePlugins = [
     replace({
         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
@@ -58,7 +60,7 @@ const plugins = [
     typescript(),
     cjs(),
     nodeGlobals(), // WARNING: Never move above CommonJS plugin!
-    isProduction ? minifyLit({
+    isProduction && minifyLit({
         include: ['src/entry.ts', 'src/{components,views}/**', 'node_modules/@polymer/{paper,iron}-*/**'],
         includeExtension: ['.ts', '.js'],
         literals: false,
@@ -66,30 +68,32 @@ const plugins = [
             minifyCSS: true, // causes some kind of trouble currently
             collapseWhitespace: true
         }
-    }) : null,
-    isProduction ? minify({ comments: false }) : null,
-    !!process.env.ROLLUP_WATCH ? browsersync({
-        port: process.env.PORT || 3000,
-        server: {
-            baseDir: dist(),
-            middleware: [historyApi()]
-        },
-        open: false,
-        ui: false
-    }) : null,
-].filter(plugin => plugin !== null);
+    }),
+];
 
 const baseOptions = {
     input: [src('index.ts'), src('views/view-party.ts'), src('views/view-tv.ts')],
     experimentalDynamicImport: true,
     experimentalCodeSplitting: true,
-    plugins,
     onwarn: err => console.error(err.toString()),
     watch: { include: 'src/**/*' },
 };
 
 export default [{
     ...baseOptions,
+    plugins: [
+        ...basePlugins,
+        // isProduction && minify({ comments: false }),
+        !!process.env.ROLLUP_WATCH && browsersync({
+            port: process.env.PORT || 3000,
+            server: {
+                baseDir: dist(),
+                middleware: [historyApi()]
+            },
+            open: false,
+            ui: false
+        }),
+    ].filter(plugin => plugin),
     output: {
         dir: dist('module'),
         format: 'es',
@@ -97,6 +101,11 @@ export default [{
     },
 }, {
     ...baseOptions,
+    plugins: [
+        ...basePlugins,
+        babel(),
+        isProduction && uglify(),
+    ].filter(plugin => plugin),
     output: {
         dir: dist('nomodule'),
         format: 'system',
