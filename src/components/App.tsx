0033746b0d65690a57dbcbb3421b5e70c7d72368
Moritz Gunz <moritz.gunz@gmail.com>
2018-12-31 16:23:38 +0100

    :sparkles: Implement app shell

---
+++ src/components/App.tsx
@@ -0,0 +1,82 @@
+import Snackbar from '@material-ui/core/es/Snackbar';
+import React, { useState } from 'react';
+import { Provider as ReduxProvider, connect } from 'react-redux';
+
+import { Views } from '../routing';
+import { isPlaybackMasterSelector } from '../selectors/party';
+import { State } from '../state';
+import { store } from '../store';
+
+import ViewHome from './ViewHome';
+
+import styles from './App.module.scss';
+
+interface AppShellProps {
+  isPlaybackMaster: boolean;
+  isToastOpen: boolean;
+  toastText: string;
+  view: Views;
+}
+
+const pageSelector = (view: Views) => {
+  switch (view) {
+    default:
+    case Views.Home:
+      return <ViewHome className={styles.viewHome} />;
+    case Views.Party:
+      return <p>Party-View</p>;
+    case Views.Tv:
+      return <p>TV-View</p>;
+  }
+};
+
+const loadOnce = (when: boolean, url: string) => {
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  if (when && !hasLoaded) {
+    setHasLoaded(true);
+
+    const script = document.createElement('script');
+    script.src = url;
+    document.body.appendChild(script);
+  }
+};
+
+const AppShellComponent: React.FC<AppShellProps> = ({
+  isPlaybackMaster,
+  isToastOpen,
+  toastText,
+  view,
+}) => {
+  loadOnce(isPlaybackMaster, 'https://sdk.scdn.co/spotify-player.js');
+
+  return (
+    <div className={styles.appShell}>
+      {pageSelector(view)}
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={isToastOpen}
+        ContentProps={{ 'aria-describedby': 'message-id' }}
+        message={toastText}
+      />
+    </div>
+  );
+};
+
+const mapStateToProps = (state: State): AppShellProps => ({
+  isPlaybackMaster: isPlaybackMasterSelector(state),
+  isToastOpen: !!state.appShell.currentToast,
+  toastText: state.appShell.currentToast || '',
+  view: (state.router!.result || { view: Views.Home }).view,
+});
+const AppShell = connect(mapStateToProps)(AppShellComponent);
+
+export default () => (
+  <ReduxProvider store={store}>
+    <AppShell />
+  </ReduxProvider>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 import './index.scss';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
